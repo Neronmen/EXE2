13a43e10bed04d489bbc7219b7bccb84c1879916
Neronmen <pnkvlog1508@gmail.com>
2025-09-24 17:44:05 +0700

    Update permission api notification

--- src/modules/notifications/controllers/createNotification.controller.ts
+++ src/modules/notifications/controllers/createNotification.controller.ts
@@ -8,6 +8,8 @@ import { GetUser } from 'src/modules/auth/guards/get-user.decorator';
 import { RolesGuard } from 'src/modules/auth/guards/roles.guard';
 import { Roles } from 'src/modules/auth/guards/roles.decorator';
 import { CreateNotificationService } from '../services/createNotification.service';
+import { PermissionGuard } from 'src/modules/auth/guards/permissions.guard';
+import { Permissions } from 'src/modules/auth/guards/permission.decorator';
 
 @ApiTags(`${resourcesV1.CREATE_NOTIFICATION.parent}`)
 @Controller(routesV1.apiversion)
@@ -17,8 +19,8 @@ export class CreateNotificationController {
   ) { }
   @ApiOperation({ summary: resourcesV1.CREATE_NOTIFICATION.displayName })
   @ApiBearerAuth()
-  @UseGuards(JWTGuard, RolesGuard)
-  @Roles(1)
+  @UseGuards(JWTGuard, PermissionGuard)
+  @Permissions("CREATE_NOTIFICATION_SYSTEM")
   @Post(routesV1.notification.root)
   async createNotification(@Body() data: CreateNotificationDto, @GetUser() user) {
     return await this.createnNotificationService.createNotification(data, user)
